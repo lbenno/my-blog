daa4f42ee81a791c437cca76aad9a949f89d69be
Lauren Benson <laurenbenson@Laurens-MBP.fritz.box>
2024-04-23 18:28:40 +0200

    Changed layout, style and createdd landing page

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,15 @@ from .models import Post, Comment
 from .forms import PostForm, CommentForm
 
 # Create your views here.
+def landing(request):
+    return render(request, 'blog/landing.html', {})
+
+
+@login_required
+def test(request):
+    return render(request, 'blog/test.html', {})
+
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
